e5dc079480a51cc25a513d5c9d667519d3440772
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-11-30 15:14:55 +0900

    01

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  struct student {
+  
+  int id;
+  char name[10];
+  float grade;
+  
+  
+};
+
+
+int main(int argc, char *argv[])
+{
+    struct student s1 ={123, "SEOYEON", 4.3};
+
+  system("PAUSE");	
+  return 0;
+}
