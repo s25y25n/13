60d8a55142fa5b36fd83341e72f8dcab6c2148d8
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-11-30 15:20:28 +0900

    1-2

--- main.c
+++ main.c
@@ -14,6 +14,13 @@
 int main(int argc, char *argv[])
 {
     struct student s1 ={123, "SEOYEON", 4.3};
+    
+    //다른값변경
+    s1.id = 456;
+    s1.name[0] = 'J';
+    s1.grade = 4.5;
+    //출력 
+    printf("id : %d, name : %s, grade: %f",s1.id, s1.name, s1.grade);
 
   system("PAUSE");	
   return 0;
